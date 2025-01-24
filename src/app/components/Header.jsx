d9d2c223b76eea53886df8efbe9db9ea750c9d10
wadsahin <wadsahinalom@gmail.com>
2025-01-25 00:31:37 +0600

    Kinde auth login added, I would try to my best but not working faced error..

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,11 @@
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LoginLink, LogoutLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-const Header = () => {
+const Header = async() => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  console.log("From header auth", isUserAuthenticated);
   return (
     <div className="navbar bg-indigo-500 text-white px-20">
       <div className="flex-1 gap-3">
@@ -10,7 +14,7 @@ const Header = () => {
       </div>
       <div className="flex-none gap-2">
         <LoginLink className="btn btn-sm btn-neutral text-white">Login</LoginLink>
-        <Link href="/" className="btn btn-error btn-sm text-white">Logout</Link>
+        {/* <LogoutLink>Logout</LogoutLink> */}
       </div>
     </div>
   );
